3c27109d07663fd78ce8ccc8934859969690a93f
shalva <shalvachkuaselidze77@gmail.com>
2020-04-24 16:55:18 +0400

    added permission module

--- app/User.php
+++ app/User.php
@@ -4,12 +4,13 @@ namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use App\Post;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
