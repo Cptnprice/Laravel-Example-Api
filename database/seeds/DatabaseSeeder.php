752d7c6c270833261c2a017b44215fb8aa701445
shalva <shalvachkuaselidze77@gmail.com>
2020-05-05 18:49:42 +0400

    added permission's and role's tables seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(PostTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
     }
 }
---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(["name" => "edit posts"]);
+        Permission::create(["name" => "delete posts"]);
+    }
+}
